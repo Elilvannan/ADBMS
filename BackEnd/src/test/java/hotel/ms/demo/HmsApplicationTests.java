9509a49dee2115218188256952fb124e3e85749d
muhannadhmnh <mrmuhannadh@gmail.com>
2022-09-18 08:51:34 +0530

    BackEnd added
    Spring boot added as backend

---
+++ BackEnd/src/test/java/hotel/ms/demo/HmsApplicationTests.java
@@ -0,0 +1,13 @@
+package hotel.ms.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
