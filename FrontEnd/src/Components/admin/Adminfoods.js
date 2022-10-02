2fc4fc7277bc846c7610a9356cb7994c2092fce2
muhannadhmnh <mrmuhannadh@gmail.com>
2022-10-02 11:23:30 +0530

    Front end Foods table
    Added a pagination table to admin to show the foods list

---
+++ FrontEnd/src/Components/admin/Adminfoods.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
+import DataTable from 'react-data-table-component';
+
+const columns = [
+    {
+        name: 'NO',
+        selector: row => row.food_id,
+        sortable: true,
+    },
+    {
+        name: 'NAME',
+        selector: row => row.food_name,
+        sortable: true,
+    },
+    {
+        name: 'CATEGORY',
+        selector: row => row.food_category,
+        sortable: true,
+    },
+    {
+        name: 'DETAILS',
+        selector: row => row.food_desc,
+        sortable: true,
+    },
+];
+
+const AdminFoodPage = () => {
+    const [foods,setFoods] = useState([]);
+    useEffect(() => {
+        Axios.get('http://localhost:8080/getFoods').then((response) => {
+            setFoods(response.data);
+        });
+    }, []);
+    return (
+        <div className='container'>
+            <div className='row'>
+                <h2>FOODS</h2>
+            </div>
+            <DataTable
+            columns={columns}
+            data={foods}
+            selectableRows
+            selectableRowsHighlight
+            pagination
+            fixedHeader
+            fixedHeaderScrollHeight='70vh'
+            highlightOnHover
+            
+          />
+        </div>
+    )
+}
+
+export default AdminFoodPage
--- FrontEnd/src/Pages/adminFoods.js
+++ FrontEnd/src/Pages/adminFoods.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import AdminFoodPage from '../Components/admin/Adminfoods'
 import AdminNav from '../Components/NavBar/AdminNav'
 
 const AdminFoods = () => {
   return (
     <div>
         <AdminNav/>
-      Admin foods
+        <AdminFoodPage/>
     </div>
   )
 }
