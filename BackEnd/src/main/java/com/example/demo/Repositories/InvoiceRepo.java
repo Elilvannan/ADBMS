b1cad475e7420cc56fd019f4b088d38e56dcb6fb
Sharanya52 <61771222+Sharanya52@users.noreply.github.com>
2022-10-08 18:12:16 +0530

    invoice_added

---
+++ BackEnd/src/main/java/com/example/demo/Repositories/InvoiceRepo.java
@@ -0,0 +1,14 @@
+package com.example.demo.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.Domain.Foods;
+
+public interface InvoiceRepo extends JpaRepository<Foods, Integer>{
+
+}
+
+
+
+
+
