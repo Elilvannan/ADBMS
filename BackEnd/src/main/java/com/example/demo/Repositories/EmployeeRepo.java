0e77f9a11a310c358d6fc2de09956fd2028afc38
muhannadhmnh <mrmuhannadh@gmail.com>
2022-10-09 21:49:14 +0530

    Change on employee rep

--- BackEnd/src/main/java/com/example/demo/Repositories/EmployeeRepo.java
+++ BackEnd/src/main/java/com/example/demo/Repositories/EmployeeRepo.java
@@ -2,9 +2,9 @@ package com.example.demo.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.Domain.Foods;
+import com.example.demo.Domain.Employee;
 
-public interface EmployeeRepo extends JpaRepository<Foods, Integer>{
+public interface EmployeeRepo extends JpaRepository<Employee, Integer>{
 
 }
 
